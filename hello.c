98339aac3c506e13967ed0fe81f39ff539f4c40e
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-04-13 20:24:39 +0100

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    char a = 0;
+    printf("\033[40;37m\nhello world....\n\033[41;37m\nhello world....\n\033[42;37m\nhello world....\n\033[43;37m\nhello world....\n\033[44;37m\nhello world....\n\033[45;37m\nhello world....\n\033[46;37m\nhello world....\n\033[47;30m\nhello world....\n");
+    
+    return 0;
+}
